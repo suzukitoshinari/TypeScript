0ea360aec5c05881f18fe226de115bee57be67af
suzukitoshinari <ay658mymt@gmail.com>
2021-03-22 02:24:49 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import { ChatEngine } from 'react-chat-engine';
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 
 const App = () => {
+  if(!localStorage.getItem('username')) return <LoginForm />
+
   return (
     <ChatEngine
       height="100vh"
       projectID="d6c185fd-66df-4993-8b4c-207bbed4333b"
-      userName="hoko"
-      userSecret="1234"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const authObject = { 'Project-ID': "d6c185fd-66df-4993-8b4c-207bbed4333b", 'User-Name': username, 'User-Secret': password };
+
+        try {
+            await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+
+            window.location.reload();
+        } catch (error) {
+            setError('Oops, incorrect CredentialsContainer.');
+        }
+    }
+
+    return (
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input className="input" placeholder="Username is hoko" required type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <input className="input" placeholder="Password is 1234" required type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    <div align="center">
+                        <button type="submit" className="button">
+                            <span>Start Chatting</span>
+                        </button>
+                    </div>
+                    <h2 className="error">{error}</h2>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default LoginForm;
